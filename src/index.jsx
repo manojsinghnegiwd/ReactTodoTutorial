a7b4fc46e830e3680e052c0d2e8bf3e2e848ff39
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-18 23:03:52 +0530

    added component

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import YourComponent from './YourComponent';
-
+import App from './App';
 
 ReactDOM.render(
-	<YourComponent />,
+	<App />,
 	document.getElementById('app')
 );
